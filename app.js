87f85d88d638382fa226484c1fcbc10e3e8a9d4f
MazzatostaMirko <mazzatostamirko@gmail.com>
2025-03-24 14:25:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let time = 16;
+let greeting = time < 12 ? "Good morning!" : "Good afternoon!";
+console.log(greeting);
+
+let isStudent = true;
+let message = isStudent ? "You are a student" : "You are not a student";
+console.log(message);
+
+let purchaseAmount = 125;
+let discount = purchaseAmount >= 100 ? 10 : 0;
+console.log(`Your total is $${purchaseAmount - purchaseAmount * (discount/100) }`)
